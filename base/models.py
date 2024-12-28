32d3633ed114a6c3f366122e69d7782ff2c2ad27
FIRST_NAME LAST_NAME <charantm8787@gmail.com>
2024-12-28 20:09:24 +0530

    added models

--- base/models.py
+++ base/models.py
@@ -1,3 +1,41 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+class CustomUser(AbstractUser):
+    bio = models.TextField(max_length=100)
+    groups = models.ManyToManyField('auth.Group', related_name='customuser_groups', blank=True)
+    user_permissions = models.ManyToManyField(
+        'auth.Permission',
+        related_name='customuser_permissions',  # Custom reverse accessor name
+        blank=True
+    )
+    profile_picture = models.ImageField(upload_to='profile_picture/', blank=True, null=True)
+
+class Room(models.Model):
+    created_by = models.ForeignKey(CustomUser, on_delete=models.CASCADE,related_name="rooms")
+    name = models.CharField(max_length=50)
+    description = models.TextField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_private = models.BooleanField(default=False)
+
+class RoomMembership(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class StudyMaterials(models.Model):
+    upload_by = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    room = models.ForeignKey(Room, on_delete=models.CASCADE, related_name="myuploads")
+    title = models.CharField(max_length=255, null=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    file = models.FileField(upload_to="study_materials/")   
+
+class ChatBox(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    room = models.ForeignKey(Room, on_delete=models.CASCADE, related_name="messages")
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
