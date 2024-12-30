1b21503f81e826f8c12d7a48a479205d1bc4f2e6
FIRST_NAME LAST_NAME <charantm8787@gmail.com>
2024-12-30 21:39:21 +0530

    alignment of the auth part

--- static/js/script.js
+++ static/js/script.js
@@ -7,6 +7,17 @@ menuBtn.addEventListener("click", () => {
 function myFunction(x) {
   x.classList.toggle("change");
 }
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-50px";
+  }
+}
 
 // window.addEventListener("load", () => {
 //   setTimeout(() => {
